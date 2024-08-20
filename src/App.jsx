96bdefb52d7fb5046c14b7f6057142adae2893bf
Alejandro Dahlgren <alejandro.dahlgren@gmail.com>
2024-08-20 13:39:37 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='banner'>
+        <div className='slider' style={{"--quantity": 10}}>
+          <div className='item' style={{ "--position": 1}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+          <div className='item' style={{ "--position": 2}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+          <div className='item' style={{ "--position": 3}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+          <div className='item' style={{ "--position": 4}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+          <div className='item' style={{ "--position": 5}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+          <div className='item' style={{ "--position": 6}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+          <div className='item' style={{ "--position": 7}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+          <div className='item' style={{ "--position": 8}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+          <div className='item' style={{ "--position": 9}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+          <div className='item' style={{ "--position": 10}}><img src="https://cmsassets.rgpub.io/sanity/images/dsfx7636/game_data_live/8201aebd324836be730d1be4dd9d5d2e7aa4004c-496x560.jpg?auto=format&fit=fill&q=80&w=352" alt="" /></div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
